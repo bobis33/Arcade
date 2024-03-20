381b1238af216a7447a58296191e079be324d528
MASINA Elliot <elliot.masina@epitech.eu>
2024-03-20 13:42:40 +0100

    [STYLE]: ncurses constructor

--- Graphics/NCurses/src/Ncurses.cpp
+++ Graphics/NCurses/src/Ncurses.cpp
@@ -5,21 +5,19 @@
 ** Ncurses.cpp
 */
 
-#include <cstring>
 #include "Arcade/Ncurses.hpp"
-
 
 Arcade::Ncurses::Ncurses()
 {
-    int index = 0;
+    int size_title = 0;
     initscr();
     raw();
     noecho();
     curs_set(0);
     keypad(stdscr, TRUE);
-    _window = newwin(20, 80, 0, 0);
-    for (; _title.data()[index]; index++);
-    const int window_position = (COLS - index) / 2;
-    mvprintw(0, window_position, "%s", _title.data());
+    _window = newwin(0, 0, 0, 0);
+    for (; _title.data()[size_title]; size_title++);
+    const int title_position = (COLS - size_title) / 2;
+    mvprintw(0, title_position, "%s", _title.data());
     wrefresh(_window);
 }
