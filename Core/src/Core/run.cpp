930502db275605681702d4da691f91c0dad2a2bd
MASINA Elliot <elliot.masina@epitech.eu>
2024-04-07 12:22:55 +0200

    [FIX]: cannot launch program with game

--- Core/src/Core/run.cpp
+++ Core/src/Core/run.cpp
@@ -5,20 +5,16 @@
 ** run.cpp
 */
 
+#include <filesystem>
+#include <ranges>
+
 #include "Arcade/Core.hpp"
 #include "Arcade/Constants.hpp"
 #include "Arcade/RuntimeException.hpp"
 
-static constexpr std::string_view NO_DISPLAY = "DISPLAY environment variable is not set";
-
-static constexpr std::string_view UNKNOW_ERROR = "Unknown error";
-static constexpr std::string_view CORE_EXCEPT = "Core exception: ";
-static constexpr std::string_view RUNTIME_EXCEPT = "Runtime exception: ";
-
 void Arcade::Core::loadRenderer(const std::string &path)
 {
     switchLib<IRenderer>(path);
-    getLibraries(path);
     if (_renderer->isASCII()) {
         _renderer->setSize(80, 40);
         _renderer->getWindow()->openWindow(80, 40);
@@ -27,26 +23,32 @@ void Arcade::Core::loadRenderer(const std::string &path)
         _renderer->getWindow()->openWindow(WIDTH, HEIGHT);
     }
     if (!_renderer->loadFont("assets/fonts/menu_i.ttf", "menu_i"))
-        throw RuntimeException("Cannot load fonts");
+        throw RuntimeException{"Cannot load fonts"};
     loadMenu();
     loadLogin();
 }
 
 int Arcade::Core::runArcade(const std::string &path)
 {
+    std::string activeLib{};
+
     try {
         if (getenv("DISPLAY") == nullptr)
-            throw CoreException{NO_DISPLAY.data()};
+            throw CoreException{"DISPLAY environment variable is not set"};
+        getLibraries(path);
+        activeLib = std::filesystem::path(path).filename().string();
+        if (std::ranges::find(_graphicLibs, activeLib) == _graphicLibs.end())
+            throw CoreException{"Must be a graphic library: " + activeLib};
         loadRenderer(path);
         mainLoop();
     } catch (const CoreException &e) {
-        std::cerr << CORE_EXCEPT << e.what() << '\n';
+        std::cerr << "Core exception: " << e.what() << '\n';
         return EPITECH_ERROR;
     } catch (const RuntimeException &e) {
-        std::cerr << RUNTIME_EXCEPT << e.what() << '\n';
+        std::cerr << "Runtime exception: " << e.what() << '\n';
         return EPITECH_ERROR;
     } catch (...) {
-        std::cerr << UNKNOW_ERROR << '\n';
+        std::cerr << "Unknown error" << '\n';
         return EPITECH_ERROR;
     }
     return SUCCESS;
